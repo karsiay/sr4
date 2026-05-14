8ab2d415f951513a8cad6f1d779e6283bb848ffe
karsiay <klochkova2607@gmail.com>
2026-05-14 19:48:47 +0000

    update

--- sr4/sr4.cpp
+++ sr4/sr4.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+    bool A, B;
+    cout << "Input two boolean values (0 or 1): ";
+    cin >> A >> B;
+    
+    bool result = (!A && B) || !A;
+    cout << "Result: " << result << endl;
+    
+    return 0;
+}
